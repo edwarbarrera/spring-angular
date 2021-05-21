d67ce6383128d32afc9f969f7a904a808bfb190c
ed <edwarbarrera@gmail.com>
2021-05-21 18:01:57 +0200

    creation class employee

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,17 @@
+import { Employee } from './../employee';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees: Employee[];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
